877dbc988fb72c34c9d06d2b457645f2950d5dcb
Calvin Rai <raicalvin@gmail.com>
2018-09-05 14:17:14 -0700

    Created empty StoreContract to store column name constants.

---
+++ app/src/main/java/com/example/inventoryapp/data/StoreContract.java
@@ -0,0 +1,9 @@
+package com.example.inventoryapp.data;
+
+public final class StoreContract {
+
+    /**
+     * Declare empty constructor to ensure the class cannot be instantiated
+     */
+    private StoreContract() {}
+}
