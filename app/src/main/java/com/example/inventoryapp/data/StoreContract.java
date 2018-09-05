b8da9b43e3a105ae417f1cbbb3e5ce9cd2811be7
Calvin Rai <raicalvin@gmail.com>
2018-09-05 14:22:50 -0700

    Updated StoreContract with column name constants.

--- app/src/main/java/com/example/inventoryapp/data/StoreContract.java
+++ app/src/main/java/com/example/inventoryapp/data/StoreContract.java
@@ -1,9 +1,24 @@
 package com.example.inventoryapp.data;
 
+import android.provider.BaseColumns;
+
 public final class StoreContract {
 
     /**
      * Declare empty constructor to ensure the class cannot be instantiated
      */
     private StoreContract() {}
+
+    public final static String TABLE_NAME = "store";
+
+    public final static String _ID = BaseColumns._ID;
+    public final static String COL_PRODUCT_NAME = "product";
+    public final static String COL_PRICE = "price";
+    public final static String COL_QUANTITY = "quantity";
+    public final static String COL_SUPPLIER_NAME = "supplier";
+    public final static String COL_SUPPLIER_PHONE = "phone";
+
+    public final static int PRODUCT_IN_STOCK = 1;
+    public final static int PRODUCT_OUT_OF_STOCK = 0;
+
 }
