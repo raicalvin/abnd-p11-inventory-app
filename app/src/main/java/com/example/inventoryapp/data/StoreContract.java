e1cb3496e029720fcf33b970c34c2897312fff9a
Calvin Rai <raicalvin@gmail.com>
2018-10-13 19:08:34 -0700

    Deleted unused constants in StoreContract.

--- app/src/main/java/com/example/inventoryapp/data/StoreContract.java
+++ app/src/main/java/com/example/inventoryapp/data/StoreContract.java
@@ -6,9 +6,7 @@ import android.provider.BaseColumns;
 
 public final class StoreContract {
 
-    /**
-     * Declare empty constructor to ensure the class cannot be instantiated
-     */
+    /** Declare empty constructor to ensure the class cannot be instantiated */
     private StoreContract() {}
 
     /**  Content Authority Creation */
@@ -16,13 +14,11 @@ public final class StoreContract {
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
     public static final String PATH_IPHONES = "iphones";
 
-
     public static final class StoreEntry implements BaseColumns {
 
         /** MIME Types */
         public static final String CONTENT_LIST_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_IPHONES;
         public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_IPHONES;
-
 
         public final static String TABLE_NAME = "iphones";
 
@@ -32,9 +28,6 @@ public final class StoreContract {
         public final static String COLUMN_QUANTITY = "quantity";
         public final static String COLUMN_SUPPLIER_NAME = "supplier";
         public final static String COLUMN_SUPPLIER_PHONE = "phone";
-
-        public final static int PRODUCT_IN_STOCK = 1;
-        public final static int PRODUCT_OUT_OF_STOCK = 0;
 
         /** Content URI to access the iPhone data in the provider */
         public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_IPHONES);
